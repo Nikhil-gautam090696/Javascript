f9852f67856002c078055792ec63c2dc0f512909
nikhilgautam <nikhilgautam69@gmail.com>
2022-07-15 00:40:49 +0530

    first initialization through vs code

---
+++ app.js
@@ -0,0 +1,17 @@
+// function Add(a, b) {
+//   console.log(a + b);
+// }
+// Add(3, 5);
+
+//How to write Table of any number
+
+function Table(num) {
+  for (let i = 1; i <= 10; i++) {
+    console.log(`${num} X ${i} = ${num * i}`);
+  }
+}
+// Table(2);
+
+// Table(4);
+
+Table(18);
