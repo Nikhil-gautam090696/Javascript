964be02618121813053d6a199c9aa3e53848a8c5
nikhilgautam <nikhilgautam69@gmail.com>
2022-07-15 15:34:32 +0530

    today's learning

--- part_26.js
+++ part_26.js
@@ -16,13 +16,15 @@ delete person.lastName;
 
 console.log(person);
 
-// How to know wheather a property assign or not
-//We use (in) operator
+// How to know wheather a property exist or not
+//We use **//*(in) operator
 
 console.log("height" in person);
 
-//iterating property of an object
+//iterating property of an object we will use for in loop
 
 for (let key in person) {
   console.log(key + ":" + person[key]);
 }
+
+//? for in loop is used to iterate over an object
--- part_27.js
+++ part_27.js
@@ -3,16 +3,59 @@
 let person = {
   firstName: "Nikhil",
   lastName: "Gautam",
+  sayHello() {
+    console.log("Hello!");
+  },
+};
+person.age = 25;
+person.sayHello();
+
+// there are 4 methods in javascript
+// <------------------------------------------------->
+//? 1st way to create a method in object
+// **Using Anonymous function**
+
+person.sayHello = function () {
+  console.log("Hello !");
 };
 
-// 1st way to create a method in object
-person.age = 25;
-// person.sayHello = function () {
-//   console.log("Hello !");
-// };
+person.sayHello();
 
-// person.sayHello();
+//<------------------------------------------------->
+//? 2nd way to create a method in object
+
+//** Using a Normal Function and then assign  */
 
 function greet() {
-  console.log("Hello !");
+  console.log("2nd way !");
 }
+person.sayHello = greet;
+person.sayHello();
+
+//<------------------------------------------------->
+
+//? 3rd way to create a method in object
+
+//** Defining the method inside the object  */
+
+let person = {
+  firstName: "Nikhil",
+  lastName: "Gautam",
+  sayHello: function () {
+    console.log("I'm 3rd way inside an object");
+  },
+};
+person.sayHello();
+
+//? 4th way to create a method in object (ES6+)
+//** Direct (without using function keyword)  */
+
+let student = {
+  firstName: "Nikhil",
+  lastName: "Gautam",
+  sayHello() {
+    console.log("I'm latest ES6+");
+  },
+};
+student.sayHello();
+console.log(student.firstName);
